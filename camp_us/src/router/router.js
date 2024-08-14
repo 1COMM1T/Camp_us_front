3ef49c7cdc3023492e173469860baf4309bfbf64
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-08-15 02:35:23 +0900

    Feat: 로그인화면 router 추가

--- camp_us/src/router/router.js
+++ camp_us/src/router/router.js
@@ -8,6 +8,10 @@ const routes = [
   {
     path: '/campingMain',
     component: () => import('../views/CampingMainView.vue')
+  },
+  {
+    path: '/login',
+    component: () => import('../views/LoginView.vue')
   }
 ]
 
@@ -17,3 +21,4 @@ const router = createRouter({
 })
 
 export default router  // 여기에서 default로 내보냅니다
+
