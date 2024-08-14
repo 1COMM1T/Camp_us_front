5fa840df445b157f9e16870706d4d38bbdd6ec20
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-08-14 20:52:55 +0900

    Chore: router 추가

--- camp_us/src/components/Header.vue
+++ camp_us/src/components/Header.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="headerAll">
         <div class="logo">
-            <h3>CAMP US</h3>
+            <h3 @click="navigateTo('/')">CAMP US</h3>
         </div>
         <div class="menu">
-            <span>캠핑장 조회</span>
+            <span @click="navigateTo('/campingMain')">캠핑장 조회</span>
             <span>찜 목록 조회</span>
             <span>리뷰 조회</span>
             <span>예약 조회</span>
@@ -16,6 +16,13 @@
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const navigateTo = (path) => {
+    router.push(path);
+};
 
 </script>
 
@@ -30,6 +37,7 @@
 .logo {
     align-self: center;
     justify-self: center;
+    cursor: pointer;
 }
 
 .logo h3{
@@ -41,7 +49,13 @@
 }
 
 .menu span{
-    margin-left: 10px;
+    margin-left: 20px;
+    cursor: pointer; 
+    transition: color 0.1s;
+}
+
+.menu span:hover {
+    color: #0056b3; 
 }
 
 .login {
